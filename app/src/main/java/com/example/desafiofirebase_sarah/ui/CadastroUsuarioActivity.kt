81c41b83d0e6de0a8fd05c17b738931f299041b5
carneirosarah <sarahrlcarneiro@gmail.com>
2021-01-27 16:12:37 -0300

    Criação das activities

---
+++ app/src/main/java/com/example/desafiofirebase_sarah/ui/CadastroUsuarioActivity.kt
@@ -0,0 +1,12 @@
+package com.example.desafiofirebase_sarah.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.desafiofirebase_sarah.R
+
+class CadastroUsuarioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_usuario)
+    }
+}
