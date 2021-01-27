bb546a6ca16a849dde92623e1f8a12fe4f5d1b11
carneirosarah <sarahrlcarneiro@gmail.com>
2021-01-27 17:22:07 -0300

    Implementação Layout Tela Login

--- app/src/main/java/com/example/desafiofirebase_sarah/ui/LoginActivity.kt
+++ app/src/main/java/com/example/desafiofirebase_sarah/ui/LoginActivity.kt
@@ -1,12 +1,22 @@
 package com.example.desafiofirebase_sarah.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.desafiofirebase_sarah.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        loginButton.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
+
+        login_tv_create_account.setOnClickListener {
+            startActivity(Intent(this, CadastroUsuarioActivity::class.java))
+        }
     }
 }
