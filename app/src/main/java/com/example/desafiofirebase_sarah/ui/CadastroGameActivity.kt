0af891020de3f473bda1914d735401a263898146
carneirosarah <sarahrlcarneiro@gmail.com>
2021-01-28 22:49:11 -0300

    Implementação da  captura de imagens

--- app/src/main/java/com/example/desafiofirebase_sarah/ui/CadastroGameActivity.kt
+++ app/src/main/java/com/example/desafiofirebase_sarah/ui/CadastroGameActivity.kt
@@ -1,12 +1,61 @@
 package com.example.desafiofirebase_sarah.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.AlertDialog
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.desafiofirebase_sarah.R
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.squareup.picasso.Picasso
+import dmax.dialog.SpotsDialog
+import kotlinx.android.synthetic.main.activity_cadastro_game.*
+
 
 class CadastroGameActivity : AppCompatActivity() {
+
+    lateinit var storageRef: StorageReference
+    lateinit var alertDialog: AlertDialog
+    private val REQUEST_IMAGE_CAPTURE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro_game)
+        config()
+
+        addPictureButton.setOnClickListener {
+            dispatchTakePictureIntent()
+        }
+    }
+
+    private fun dispatchTakePictureIntent() {
+
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            addPictureButton.setImageBitmap(imageBitmap)
+        }
+    }
+
+
+    fun config() {
+
+        storageRef = FirebaseStorage.getInstance().getReference("image")
     }
 }
