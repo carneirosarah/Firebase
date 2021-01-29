4a65bd1a640cfde313b384d522231ed873c587c2
carneirosarah <sarahrlcarneiro@gmail.com>
2021-01-29 20:28:41 -0300

    Inserção de dados concluída

--- app/src/main/java/com/example/desafiofirebase_sarah/ui/CadastroGameActivity.kt
+++ app/src/main/java/com/example/desafiofirebase_sarah/ui/CadastroGameActivity.kt
@@ -1,6 +1,5 @@
 package com.example.desafiofirebase_sarah.ui
 
-import android.app.AlertDialog
 import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
@@ -9,18 +8,21 @@ import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.desafiofirebase_sarah.R
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
-import com.squareup.picasso.Picasso
-import dmax.dialog.SpotsDialog
 import kotlinx.android.synthetic.main.activity_cadastro_game.*
+import java.io.ByteArrayOutputStream
 
 
 class CadastroGameActivity : AppCompatActivity() {
 
-    lateinit var storageRef: StorageReference
-    lateinit var alertDialog: AlertDialog
+    private lateinit var storageRef: StorageReference
+    private lateinit var newData: ByteArray
     private val REQUEST_IMAGE_CAPTURE = 1
+    private lateinit var db: FirebaseFirestore
+    private lateinit var cr: CollectionReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -30,6 +32,10 @@ class CadastroGameActivity : AppCompatActivity() {
 
         addPictureButton.setOnClickListener {
             dispatchTakePictureIntent()
+        }
+
+        saveGameButton.setOnClickListener {
+            uploadData()
         }
     }
 
@@ -50,12 +56,62 @@ class CadastroGameActivity : AppCompatActivity() {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             val imageBitmap = data?.extras?.get("data") as Bitmap
             addPictureButton.setImageBitmap(imageBitmap)
+            val baos = ByteArrayOutputStream()
+            imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+            newData = baos.toByteArray()
+
         }
     }
 
+    fun getData(url: String): MutableMap<String, Any> {
+
+        val game: MutableMap<String, Any> = HashMap()
+
+        game["name"] = game_name.text.toString()
+        game["create_at"] = game_created_at.text.toString()
+        game["desc"] = game_desc.text.toString()
+        game["img"] = url
+
+        return game
+    }
+
+    fun uploadData () {
+
+        val name = game_name.text.toString()
+        val imgRef = storageRef.child("images/" + name + ".jpg")
+        val uploadTask = imgRef.putBytes(newData)
+        val task = uploadTask.continueWithTask { task ->
+
+            if (task.isSuccessful) {
+                Toast.makeText(this, "Upload da imagem em progresso", Toast.LENGTH_SHORT).show()
+            }
+
+            imgRef!!.downloadUrl
+        }.addOnCompleteListener { task ->
+
+            if (task.isSuccessful) {
+                val downloadUri = task.result
+                val url = downloadUri.toString()
+                        .substring(0, downloadUri.toString().indexOf("&token"))
+                Log.i("Link Direto ", url)
+
+                val data = getData(url)
+
+                cr.document(name).set(data).addOnSuccessListener {
+
+                    Toast.makeText(this, "Upload dos dados realizado com sucesso", Toast.LENGTH_SHORT).show()
+                }.addOnFailureListener {
+
+                    Log.i("Cadastro Game", it.toString())
+                }
+            }
+        }
+    }
 
     fun config() {
 
-        storageRef = FirebaseStorage.getInstance().getReference("image")
+        storageRef = FirebaseStorage.getInstance().reference
+        db = FirebaseFirestore.getInstance()
+        cr = db.collection("games")
     }
 }
