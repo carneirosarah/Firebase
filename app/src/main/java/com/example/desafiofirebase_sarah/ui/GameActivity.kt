87f7674e68fbe88097feb027700412ed8e168a88
carneirosarah <sarahrlcarneiro@gmail.com>
2021-01-29 22:10:35 -0300

    Adição dos dados na tela game

--- app/src/main/java/com/example/desafiofirebase_sarah/ui/GameActivity.kt
+++ app/src/main/java/com/example/desafiofirebase_sarah/ui/GameActivity.kt
@@ -3,6 +3,8 @@ package com.example.desafiofirebase_sarah.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.desafiofirebase_sarah.R
+import com.example.desafiofirebase_sarah.domain.Game
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_game.*
 
 class GameActivity : AppCompatActivity() {
@@ -13,5 +15,12 @@ class GameActivity : AppCompatActivity() {
         game_return.setOnClickListener{
             finish()
         }
+
+        var game = intent.extras?.get("game") as Game
+        Picasso.get().load(game.image).into(game_image)
+        movie_name.text = game.name
+        movie_lancamento.text = "Lançamento: " + game.anoLancamento
+        movie_desc.text = game.descricao
+
     }
 }
